49965cffb9c472ac3f9fc1e6ab492992640d7d7a
wmi338 <b8932023@gmail.com>
2026-09-19 16:25:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'transcribe-pwa-v0.5.0'
+const CACHE_NAME = 'transcribe-pwa-v0.5.1'
 const APP_SHELL = ['./', './manifest.webmanifest']
 
 self.addEventListener('install', (event) => {
